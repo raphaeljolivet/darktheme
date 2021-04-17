9f299c23207a24dea5322ccefe37358031014f27
bl-themes <81474171+bl-themes@users.noreply.github.com>
2021-04-18 00:25:04 +0800

    Update page.php

--- php/page.php
+++ php/page.php
@@ -20,8 +20,6 @@
             
             <div class="col-sm-9">
                 <p class="text-secondary"><?php echo $page->content(); ?></p>
-                <?php Theme::plugins('pageEnd'); ?>
-                <br>
                 <?php if (!empty($page->tags(true))): ?>
                 <p class="pt-3">
                     <?php foreach ($page->tags(true) as $tagKey=>$tagName): ?>
@@ -29,6 +27,8 @@
                     <?php endforeach ?>
                 </p>
                 <?php endif; ?>
+                <br>
+                 <?php Theme::plugins('pageEnd'); ?>
             </div>
             
         </div>
